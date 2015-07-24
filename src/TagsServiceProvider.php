dd1ab9c1f4828ffbf5273aa9ba00a1288a1982ee
Bruno Gaspar <brunofgaspar1@gmail.com>
2015-07-24 19:22:37 +0100

    Added a service provider to allow migrations publishing.

---
+++ src/TagsServiceProvider.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * Part of the Tags package.
+ *
+ * NOTICE OF LICENSE
+ *
+ * Licensed under the 3-clause BSD License.
+ *
+ * This source file is subject to the 3-clause BSD License that is
+ * bundled with this package in the LICENSE file.
+ *
+ * @package    Tags
+ * @version    2.0.1
+ * @author     Cartalyst LLC
+ * @license    BSD License (3-clause)
+ * @copyright  (c) 2011-2015, Cartalyst LLC
+ * @link       http://cartalyst.com
+ */
+
+namespace Cartalyst\Tags;
+
+use Illuminate\Support\ServiceProvider;
+
+class TagsServiceProvider extends ServiceProvider
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function boot()
+    {
+        $this->publishes([
+            realpath(__DIR__.'/../resources/migrations') => database_path('migrations'),
+        ], 'migrations');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function register()
+    {
+        //
+    }
+}
